94ba4784d6fdbfd302caf0c4bf1c618271d43dd2
Daniel Podolsky <78098754+DanielPodolsky@users.noreply.github.com>
2025-01-17 20:51:01 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Costs-Management-System/',
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx']
-  }
+  build: {
+    rollupOptions: {
+      input: 'src/main.jsx'
+    }
 })
