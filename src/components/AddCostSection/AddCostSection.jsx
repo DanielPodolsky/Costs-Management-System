70da4d708a48505aac6e6e03dd9d87a5aa934bfd
Daniel Podolsky <danielpodolsky2@gmail.com>
2025-01-18 00:17:32 +0200

    Testing for Github pages

--- src/components/AddCostSection/AddCostSection.jsx
+++ src/components/AddCostSection/AddCostSection.jsx
@@ -1,123 +1,140 @@
-import './AddCostSection.css'
-import { useState } from 'react';
-import { dbService } from '../../scripts/database.js';
-import { TextField, Select, MenuItem, Button, Box, FormControl, InputLabel } from '@mui/material';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import "./AddCostSection.css";
+import { useState } from "react";
+import { dbService } from "../../scripts/database.js";
+import {
+  TextField,
+  Select,
+  MenuItem,
+  Button,
+  Box,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
+import dayjs from "dayjs"; // Import dayjs for date handling
 
 function AddCostSection() {
+  const [formData, setFormData] = useState({
+    amount: "",
+    category: "",
+    description: "",
+    date: null, // Initialize with null for dayjs compatibility
+    paymentMethod: "",
+  });
 
-    const [formData, setFormData] = useState({
-        amount: '',
-        category: '',
-        description: '',
-        date: '',
-        paymentMethod: ''
-    });
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // To prevent the browser from refreshing as it is the default behavior.
 
-    const handleSubmit = async (e) => {
-        e.preventDefault(); // To prevent the browser from refreshing as it is the default behavior.
-
-        try {
-            await dbService.addCost(formData); // formData should be with information because each input has onChange attribute.
-            //! After adding the cost, reset the form.
-            setFormData({
-                amount: '',
-                category: '',
-                description: '',
-                date: '',
-                paymentMethod: ''
-            });
-            alert('Cost added successfully!');
-        } catch (error) {
-            console.error('Error adding cost:', error);
-            alert('Failed to add cost. Please try again.');
-        }
+    try {
+      await dbService.addCost(formData); // formData should be with information because each input has onChange attribute.
+      //! After adding the cost, reset the form.
+      setFormData({
+        amount: "",
+        category: "",
+        description: "",
+        date: null, // Reset date to null
+        paymentMethod: "",
+      });
+      alert("Cost added successfully!");
+    } catch (error) {
+      console.error("Error adding cost:", error);
+      alert("Failed to add cost. Please try again.");
     }
+  };
 
+  //! Detects change in input fields
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-    //! Detects change in whatever input tag and updates the formData.
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prev => ({
-            ...prev,
-            [name]: value
-        }));
-    };
+  //! Detects change for DatePicker
+  const handleDateChange = (newValue) => {
+    setFormData((prev) => ({
+      ...prev,
+      date: newValue ? dayjs(newValue).format("MM-YYYY") : null, // Set to dayjs object or null
+    }));
+  };
 
+  return (
+    <section id="addCostSection">
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          "& .MuiFormControl-root": { mb: 2, width: "100%" },
+        }}
+      >
+        <TextField
+          label="Amount"
+          type="number"
+          required
+          onChange={handleInputChange}
+          name="amount"
+          value={formData.amount}
+        />
 
+        <FormControl required>
+          <InputLabel>Category</InputLabel>
+          <Select
+            label="Category"
+            onChange={handleInputChange}
+            name="category"
+            value={formData.category}
+          >
+            <MenuItem value="">Select a category</MenuItem>
+            <MenuItem value="groceries">Groceries</MenuItem>
+            <MenuItem value="utilities">Utilities</MenuItem>
+            <MenuItem value="transportation">Transportation</MenuItem>
+            <MenuItem value="entertainment">Entertainment</MenuItem>
+            <MenuItem value="healthcare">Healthcare</MenuItem>
+            <MenuItem value="education">Education</MenuItem>
+          </Select>
+        </FormControl>
 
-    return (
-        <>
-            <section id="addCostSection">
-                <Box
-                    component="form"
-                    onSubmit={handleSubmit}
-                    sx={{
-                        '& .MuiFormControl-root': { mb: 2, width: '100%' }
-                    }}
-                >
-                    <TextField
-                        label="Amount"
-                        type="number"
-                        required
-                        onChange={handleChange}
-                        name="amount"
-                        value={formData.amount}
-                    />
+        <TextField
+          label="Description"
+          multiline
+          rows={4}
+          required
+          onChange={handleInputChange}
+          name="description"
+          value={formData.description}
+        />
 
-                    <FormControl required>
-                        <InputLabel>Category</InputLabel>
-                        <Select label="Category" onChange={handleChange} name="category" value={formData.category}>
-                            <MenuItem value="">Select a category</MenuItem>
-                            <MenuItem value="groceries">Groceries</MenuItem>
-                            <MenuItem value="utilities">Utilities</MenuItem>
-                            <MenuItem value="transportation">Transportation</MenuItem>
-                            <MenuItem value="entertainment">Entertainment</MenuItem>
-                            <MenuItem value="healthcare">Healthcare</MenuItem>
-                            <MenuItem value="education">Education</MenuItem>
-                        </Select>
-                    </FormControl>
+        <TextField
+          label="Date"
+          type="date"
+          required
+          onChange={handleInputChange}
+          name="date"
+          value={formData.date}
+        />
 
-                    <TextField
-                        label="Description"
-                        multiline
-                        rows={4}
-                        required
-                        onChange={handleChange}
-                        name="description"
-                        value={formData.description}
-                    />
+        <FormControl required>
+          <InputLabel>Payment Method</InputLabel>
+          <Select
+            label="Payment Method"
+            onChange={handleInputChange}
+            name="paymentMethod"
+            value={formData.paymentMethod}
+          >
+            <MenuItem value="">Select payment method</MenuItem>
+            <MenuItem value="cash">Cash</MenuItem>
+            <MenuItem value="credit">Credit Card</MenuItem>
+            <MenuItem value="debit">Debit Card</MenuItem>
+            <MenuItem value="transfer">Bank Transfer</MenuItem>
+          </Select>
+        </FormControl>
 
-
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker label="Date *" />
-                    </LocalizationProvider>
-                    
-                    <FormControl required>
-                        <InputLabel>Payment Method</InputLabel>
-                        <Select label="Payment Method" onChange={handleChange} name="paymentMethod" value={formData.paymentMethod}>
-                            <MenuItem value="">Select payment method</MenuItem>
-                            <MenuItem value="cash">Cash</MenuItem>
-                            <MenuItem value="credit">Credit Card</MenuItem>
-                            <MenuItem value="debit">Debit Card</MenuItem>
-                            <MenuItem value="transfer">Bank Transfer</MenuItem>
-                        </Select>
-                    </FormControl>
-
-                    <Button 
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                    >
-                        Add Cost
-                    </Button>
-                </Box>
-            </section>
-        </>
-    )
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+          Add Cost
+        </Button>
+      </Box>
+    </section>
+  );
 }
 
 export default AddCostSection;
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Costs-Management-System/',
+  base: "/Costs-Management-System/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'src/main.jsx'
-    }
-})
+});
