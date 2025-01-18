176bbb2423c31d238a377eea246146d13700cf09
DanielPodolsky <danielpodolsky2@gmail.com>
2025-01-18 18:36:24 +0200

    Deploying

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Costs-Management-System/',
+  base: '/Costs-Management-System',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'src/main.jsx'
-    }
-  }
 })
